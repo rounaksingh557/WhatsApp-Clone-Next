693caceab4cc406fb37d1ab9240fad907880277f
Rounak Singh <rounaksingh9431738764@gmail.com>
2021-09-01 21:49:45 +0530

    completed the sidebar component 1/3 and time for a bit of firebase

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,12 +1,29 @@
-import { Avatar } from "@material-ui/core";
+import { Avatar, Button, IconButton } from "@material-ui/core";
 import styled from "styled-components";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import ChatIcon from "@material-ui/icons/Chat";
+import SearchIcon from "@material-ui/icons/Search";
 
 function Sidebar() {
   return (
     <Container>
       <Header>
         <UserAvatar />
+        <IconsContainer>
+          <IconButton>
+            <ChatIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        </IconsContainer>
       </Header>
+      <Search>
+        <SearchIcon />
+        <SearchInput placeholder="Search in chats" />
+      </Search>
+      <SidebarButton>Start a New chat</SidebarButton>
+      {/* Lists of Chats */}
     </Container>
   );
 }
@@ -14,5 +31,41 @@ function Sidebar() {
 export default Sidebar;
 
 const Container = styled.div``;
-const Header = styled.div``;
-const UserAvatar = styled(Avatar)``;
+const Search = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 20px;
+  border-radius: 2px;
+`;
+const SidebarButton = styled(Button)`
+  /* !NOTE: very Important */
+  width: 100%;
+  &&& {
+    border-top: 1px solid whitesmoke;
+    border-bottom: 1px solid whitesmoke;
+  }
+`;
+const SearchInput = styled.input`
+  outline-width: 0;
+  border: none;
+  flex: 1;
+`;
+const Header = styled.div`
+  display: flex;
+  position: sticky;
+  top: 0;
+  background-color: white;
+  z-index: 1;
+  justify-content: space-between;
+  align-items: center;
+  padding: 15px;
+  height: 80px;
+  border-bottom: 1px solid whitesmoke;
+`;
+const UserAvatar = styled(Avatar)`
+  cursor: pointer;
+  :hover {
+    opacity: 0.8;
+  }
+`;
+const IconsContainer = styled.div``;

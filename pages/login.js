f532ea9f1f21233249cce82f97a4f8d1db052370
Rounak Singh <rounaksingh9431738764@gmail.com>
2021-09-04 15:10:21 +0530

    new login image

--- pages/login.js
+++ pages/login.js
@@ -13,7 +13,7 @@ function Login() {
         <title>Login</title>
       </Head>
       <LoginContainer>
-        <Logo src="http://assets.stickpng.com/images/580b57fcd9996e24bc43c543.png" />
+        <Logo src="https://logo-logos.com/wp-content/uploads/2016/10/WhatsApp_logo_icon.png" />
         <Button onClick={signIn}>Sign in with Google</Button>
       </LoginContainer>
     </Container>
